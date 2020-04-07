63bd1effda6ecfe309487d1e31df26d97b302f27
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2020-04-07 20:48:58 +0300

    feature(#37): Implemented eureka server

---
+++ eureka-discovery-service/src/test/java/ru/cleverhause/eurekadiscoveryservice/CleverhauseEurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cleverhause.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverhauseEurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
