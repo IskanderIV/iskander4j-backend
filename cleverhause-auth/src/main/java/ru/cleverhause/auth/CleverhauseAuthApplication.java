d6fef660fda9fa7b205ab007277bce00c850f962
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2020-04-07 23:01:21 +0300

    feature(#37): Made cleverhause-auth app as uereka client

--- cleverhause-auth/src/main/java/ru/cleverhause/auth/CleverhauseAuthApplication.java
+++ cleverhause-auth/src/main/java/ru/cleverhause/auth/CleverhauseAuthApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CleverhauseAuthApplication {
 
 	public static void main(String[] args) {
