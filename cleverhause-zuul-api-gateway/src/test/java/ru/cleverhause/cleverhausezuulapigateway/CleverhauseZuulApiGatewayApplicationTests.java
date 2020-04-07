1b3155abeee5a28b96143a0a0d2db2365760c85d
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2020-04-08 00:25:14 +0300

    feature(#37): Implemented cleverhause-zuul-api-gateway

---
+++ cleverhause-zuul-api-gateway/src/test/java/ru/cleverhause/cleverhausezuulapigateway/CleverhauseZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cleverhause.cleverhausezuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverhauseZuulApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
