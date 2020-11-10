9cc4756909b1ace56758fd0a01c87cad28e2c855
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2020-11-10 19:06:47 +0300

    feature(#64): Repaired mongo testcontainer

--- cleverhause-devices/src/test/java/ru/cleverhause/devices/testcontainer/MongoContainerWrapper.java
+++ cleverhause-devices/src/test/java/ru/cleverhause/devices/testcontainer/MongoContainerWrapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Slf4j
 public class MongoContainerWrapper extends MongoDBContainer {
-    private static final String MONGO_IMAGE_NAME = "mongo:4.0.2";
+    private static final String MONGO_IMAGE_NAME = "mongo:4.2.10";
 
     public MongoContainerWrapper() {
         super(MONGO_IMAGE_NAME);
