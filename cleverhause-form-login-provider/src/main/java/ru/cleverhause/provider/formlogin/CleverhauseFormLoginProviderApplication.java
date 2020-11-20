14e97aba061a10f1a93e93edcb7384929d4d832a
Aleksandr A. Ivanov <aivanov10@alfabank.ru>
2020-11-20 13:57:27 +0300

    feature(#68): Implemented form login provider module

---
+++ cleverhause-form-login-provider/src/main/java/ru/cleverhause/provider/formlogin/CleverhauseFormLoginProviderApplication.java
@@ -0,0 +1,13 @@
+package ru.cleverhause.provider.formlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverhauseFormLoginProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverhauseFormLoginProviderApplication.class, args);
+	}
+
+}
---
+++ cleverhause-form-login-provider/src/test/java/ru/cleverhause/provider/formlogin/CleverhauseFormLoginProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cleverhause.provider.formlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverhauseFormLoginProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
